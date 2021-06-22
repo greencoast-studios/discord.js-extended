2b2c899523dd87aa796a4f8303ca754c49de153e
moonstar-x <christian.lopez99@outlook.com>
2021-06-21 21:48:32 -0500

    Added abstract DataProvider.

---
+++ src/classes/data/DataProvider.ts
@@ -0,0 +1,94 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Discord from 'discord.js';
+import ExtendedClient from '../ExtendedClient';
+
+/**
+ * Abstract DataProvider, it contains all the methods that need to be implemented for any DataProvider with a custom backend.
+ */
+abstract class DataProvider {
+  public readonly client: ExtendedClient;
+
+  /**
+   * @param client The client that this provider will serve for.
+   */
+  constructor(client: ExtendedClient) {
+    this.client = client;
+  }
+
+  /**
+   * Initialize the data provider.
+   * @returns A promise that resolves the data provider once it's ready.
+   */
+  public abstract init(): Promise<DataProvider>;
+
+  /**
+   * Gracefully destroy the data provider. This should close any connections. The instance will be unusable after calling this method.
+   * @returns A promise that resolves once the provider is destroyed.
+   */
+  public abstract destroy(): Promise<void>;
+
+  /**
+   * Get a value for a key in a guild.
+   * @param guild The guild for which the data will be queried.
+   * @param key The key of the data to be queried.
+   * @param defaultValue The default value in case there is no entry found.
+   * @returns A promise that resolves the queried data.
+   */
+  public abstract get(guild: Discord.Guild, key: string, defaultValue?: any): Promise<any>;
+
+  /**
+   * Get a value for a key in a global scope.
+   * @param key The key of the data to be queried.
+   * @param defaultValue The default value in case there is no entry found.
+   * @returns A promise that resolves the queried data.
+   */
+  public abstract getGlobal(key: string, defaultValue?: any): Promise<any>;
+
+  /**
+   * Set a value for a key in a guild.
+   * @param guild The guild for which the data will be set.
+   * @param key The key of the data to be set.
+   * @param value The value to set.
+   * @returns A promise that resolves once the data is saved.
+   */
+  public abstract set(guild: Discord.Guild, key: string, value: any): Promise<void>;
+
+  /**
+   * Set a value for a key in a global scope.
+   * @param key The key of the data to be set.
+   * @param value The value to set.
+   * @returns A promise that resolves once the data is saved.
+   */
+  public abstract setGlobal(key: string, value: any): Promise<void>;
+
+  /**
+   * Delete a key-value pair in a guild.
+   * @param guild The guild for which the key-value pair will be deleted.
+   * @param key The key to delete.
+   * @returns A promise that resolves the data that was deleted.
+   */
+  public abstract delete(guild: Discord.Guild, key: string): Promise<any>;
+
+  /**
+   * Delete a key-value pair in a global scope.
+   * @param key The key to delete.
+   * @returns A promise that resolves the data that was deleted.
+   */
+  public abstract deleteGlobal(key: string): Promise<any>;
+
+  /**
+   * Clear all data in a guild.
+   * @param guild The guild to clear the data from.
+   * @returns A promise that resolves once all data is deleted.
+   */
+  public abstract clear(guild: Discord.Guild): Promise<void>;
+
+  /**
+   * Clear all data in a global scope.
+   * @returns A promise that resolves once all data is deleted.
+   */
+  public abstract clearGlobal(): Promise<void>;
+}
+
+export default DataProvider;
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import Templater from './classes/abstract/Templater';
 import PresenceTemplater from './classes/presence/PresenceTemplater';
 import PresenceManager from './classes/presence/PresenceManager';
 import ConfigProvider from './classes/config/ConfigProvider';
+import DataProvider from './classes/data/DataProvider';
 
 import ExtendedClientOptions from './interfaces/ExtendedClientOptions';
 import PresenceManagerOptions from './interfaces/PresenceManagerOptions';
@@ -17,6 +18,7 @@ export {
   PresenceTemplater,
   PresenceManager,
   ConfigProvider,
+  DataProvider,
   ExtendedClientOptions,
   PresenceManagerOptions,
   PresenceData,
