2d8e5e027e38d70f413cea770a7b9438f7c77336
moonstar-x <christian.lopez99@outlook.com>
2021-06-24 23:29:31 -0500

    Updated example.

--- example/index.js
+++ example/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const path = require('path');
 const logger = require('@greencoast/logger');
-const { ExtendedClient, ConfigProvider, LevelDataProvider } = require('@greencoast/discord.js-extended');
+const { ExtendedClient, ConfigProvider } = require('@greencoast/discord.js-extended');
+const LevelDataProvider = require('@greencoast/discord.js-extended/dist/providers/LevelDataProvider').default;
 
 const config = new ConfigProvider({ env: process.env, configPath: path.join(__dirname, './config/settings.json') });
 
