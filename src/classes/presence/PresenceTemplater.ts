de7f9191ba8174d350f9c8c2b659ca1988983ffc
moonstar-x <christian.lopez99@outlook.com>
2022-01-06 18:14:15 -0500

    Turned PresenceTemplater into an AsyncTemplater.

--- src/classes/presence/PresenceTemplater.ts
+++ src/classes/presence/PresenceTemplater.ts
@@ -3,13 +3,13 @@ import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 import ExtendedClient from '../ExtendedClient';
-import Templater from '../abstract/Templater';
+import AsyncTemplater from '../abstract/AsyncTemplater';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 /**
- * A templater class to help with the application of templates for presence statuses.
+ * An async templater class to help with the application of templates for presence statuses.
  *
  * Supported templates:
  *
@@ -19,22 +19,22 @@ dayjs.extend(timezone);
  * | `{prefix}`       | Get the prefix used by the client.                                                                                          |
  * | `{cur_time}`     | Get the current time in the client's locale.                                                                                |
  * | `{owner_name}`   | Get the owner's name. If no owner is specified then it returns `undefined`.                                                 |
- * | `{client_name}`  | Get the client's name. If the client's user is not ready yet then it returns `undefined`.                                   |
+ * | `{client_name}`  | Get the client's name. If the client's user is not ready yet, then it returns `undefined`.                                   |
  * | `{uptime}`       | Get the client's uptime since last ready event emitted in a human-readable shape. Returns `null` if no uptime is available. |
  * | `{ready_time}`   | Get the client's time at which the last ready event was emitted. Returns `null` if no `readyAt` timestamp is available.     |
  * | `{num_members}`  | Get the total number of members across all the guilds that the client is connected to.                                      |
  * | `{num_commands}` | Get the number of commands registered to this client.                                                                       |
  */
-class PresenceTemplater extends Templater {
+class PresenceTemplater extends AsyncTemplater {
   /**
-   * The client that this presence templater will use as a data source.
+   * The client that this presence async templater will use as a data source.
    * @type {ExtendedClient}
    * @memberof PresenceTemplater
    */
   public readonly client: ExtendedClient;
 
   /**
-   * @param client The client that this presence templater will use as a data source.
+   * @param client The client that this presence async templater will use as a data source.
    */
   constructor(client: ExtendedClient) {
     super([
@@ -52,7 +52,7 @@ class PresenceTemplater extends Templater {
     this.client = client;
   }
 
-  public get(key: string): string {
+  public get(key: string): Promise<string> {
     switch (key) {
       case 'num_guilds':
         return this.getNumberOfGuilds();
@@ -73,96 +73,98 @@ class PresenceTemplater extends Templater {
       case 'num_commands':
         return this.getNumberOfCommands();
       default:
-        throw new Error('Unknown key inserted in PresenceTemplater.');
+        return Promise.reject(new Error('Unknown key inserted in PresenceTemplater.'));
     }
   }
 
   /**
    * Get the number of guilds the client is connected to.
-   * @returns The number of [guilds](https://discord.js.org/#/docs/main/stable/class/Guild).
+   * @returns A promise that resolves to the number of [guilds](https://discord.js.org/#/docs/main/stable/class/Guild).
    */
-  private getNumberOfGuilds(): string {
-    return this.client.guilds.cache.reduce((sum) => sum + 1, 0).toString();
+  private getNumberOfGuilds(): Promise<string> {
+    return Promise.resolve(this.client.guilds.cache.reduce((sum) => sum + 1, 0).toString());
   }
 
   /**
    * Get the prefix used by the client.
-   * @returns The client's prefix.
+   * @returns A promise that resolves to the client's prefix.
    */
-  private getPrefix(): string {
-    return this.client.prefix;
+  private getPrefix(): Promise<string> {
+    return Promise.resolve(this.client.prefix);
   }
 
   /**
    * Get the current time in the client's locale.
-   * @returns The current time.
+   * @returns A promise that resolves to the current time.
    */
-  private getCurrentTime(): string {
-    return dayjs(new Date().getTime()).tz().format('hh:mm:ss A');
+  private getCurrentTime(): Promise<string> {
+    return Promise.resolve(dayjs(new Date().getTime()).tz().format('hh:mm:ss A'));
   }
 
   /**
    * Get the owner's name. If no owner is specified then it returns `undefined`.
-   * @returns The owner's name.
+   * @returns A promise that resolves to the owner's name.
    */
-  private getOwnerName(): string {
-    return this.client.owner?.username || 'undefined';
+  private getOwnerName(): Promise<string> {
+    return Promise.resolve(this.client.owner?.username || 'undefined');
   }
 
   /**
    * Get the client's name. If the client's user is not ready yet, then it returns `undefined`.
-   * @returns The client's name.
+   * @returns A promise that resolves to the client's name.
    */
-  private getClientName(): string {
-    return this.client.user?.username || 'undefined';
+  private getClientName(): Promise<string> {
+    return Promise.resolve(this.client.user?.username || 'undefined');
   }
 
   /**
    * Get the client's uptime since last ready event emitted in a human-readable shape.
    * Returns `null` if no uptime is available.
-   * @returns The client's uptime.
+   * @returns A promise that resolves to the client's uptime.
    */
-  private getUptime(): string {
+  private getUptime(): Promise<string> {
     if (!this.client.uptime) {
-      return 'null';
+      return Promise.resolve('null');
     }
 
-    return humanizeDuration(this.client.uptime, {
+    const uptime = humanizeDuration(this.client.uptime, {
       largest: 3,
       units: ['d', 'h', 'm'],
       round: true,
       conjunction: ' and ',
       serialComma: false
     });
+
+    return Promise.resolve(uptime);
   }
 
   /**
    * Get the client's time at which the last ready event was emitted. Returns `null`
    * if no `readyAt` timestamp is available.
-   * @returns The time the client has gone ready.
+   * @returns A promise that resolves to the time the client has gone ready.
    */
-  private getReadyTime(): string {
+  private getReadyTime(): Promise<string> {
     if (!this.client.readyTimestamp) {
-      return 'null';
+      return Promise.resolve('null');
     }
 
-    return dayjs(this.client.readyTimestamp).tz().format('ddd, DD/MM/YY @HH:MM:A');
+    return Promise.resolve(dayjs(this.client.readyTimestamp).tz().format('ddd, DD/MM/YY @HH:MM:A'));
   }
 
   /**
    * Get the total number of members across all the guilds that the client is connected to.
-   * @returns The number of [members](https://discord.js.org/#/docs/main/stable/class/GuildMember).
+   * @returns A promise that resolves to the number of [members](https://discord.js.org/#/docs/main/stable/class/GuildMember).
    */
-  private getNumberOfMembers(): string {
-    return this.client.guilds.cache.reduce((sum, guild) => sum + guild.memberCount, 0).toString();
+  private getNumberOfMembers(): Promise<string> {
+    return Promise.resolve(this.client.guilds.cache.reduce((sum, guild) => sum + guild.memberCount, 0).toString());
   }
 
   /**
    * Get the number of commands registered to this client.
-   * @returns The number of commands.
+   * @returns A promise that resolves to the number of commands.
    */
-  private getNumberOfCommands(): string {
-    return this.client.registry.commands.reduce((sum) => sum + 1, 0).toString();
+  private getNumberOfCommands(): Promise<string> {
+    return Promise.resolve(this.client.registry.commands.reduce((sum) => sum + 1, 0).toString());
   }
 }
 
