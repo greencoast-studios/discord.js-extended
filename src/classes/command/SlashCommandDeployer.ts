ac805dce7130383712c4df41710ba69cad1aa9fd
moonstar-x <christian.lopez99@outlook.com>
2022-01-07 22:17:26 -0500

    Added documentation comments in SlashCommandDeployer and Validator.

--- src/classes/command/SlashCommandDeployer.ts
+++ src/classes/command/SlashCommandDeployer.ts
@@ -2,11 +2,29 @@ import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 import ExtendedClient from '../ExtendedClient';
 
+/**
+ * A class to deploy slash commands to Discord. It can deploy globally or to a single
+ * guild. Keep in mind that deploying globally can take up to 1 hour for changes to take effect,
+ * while deploying for a single guild is immediate.
+ */
 class SlashCommandDeployer {
+  /**
+   * The client that this deployer will use.
+   * @type {ExtendedClient}
+   * @memberof SlashCommandDeployer
+   */
   public readonly client: ExtendedClient;
 
+  /**
+   * A rest client to interact with the Discord API.
+   * @type {REST}
+   * @memberof SlashCommandDeployer
+   */
   public readonly rest: REST;
 
+  /**
+   * @param client The client that this deployer will use.
+   */
   constructor(client: ExtendedClient) {
     this.client = client;
     this.rest = new REST({ version: '9' }).setToken(this.client.token!);
@@ -16,6 +34,13 @@ class SlashCommandDeployer {
     }
   }
 
+  /**
+   * Deploy the commands registered to this deployer's client registry globally. Keep in mind
+   * that changes may take up to 1 hour to take effect.
+   * @returns A promise that resolves once all the commands have been deployed.
+   * @emits `client#commandDeployed`
+   * @emits `client#error`
+   */
   public async deployGlobally(): Promise<void> {
     const commands = this.client.registry.getSlashCommands();
     const commandBodies = commands.flatMap((command) => command.getAllDataBuilders());
@@ -30,6 +55,13 @@ class SlashCommandDeployer {
       });
   }
 
+  /**
+   * Deploy the commands registered to this deployer's client registry to a given guild.
+   * @param guildID The ID of the [guild](https://discord.js.org/#/docs/main/stable/class/Guild) to deploy the commands to.
+   * @returns A promise that resolves once all the commands have been deployed.
+   * @emits `client#commandDeployed`
+   * @emits `client#error`
+   */
   public deployToGuild(guildID: string): Promise<void> {
     const commands = this.client.registry.getSlashCommands();
     const commandBodies = commands.flatMap((command) => command.getAllDataBuilders());
@@ -44,10 +76,24 @@ class SlashCommandDeployer {
       });
   }
 
+  /**
+   * Deploy the commands registered to this deployer's client registry to multiple guilds.
+   * @param guildIDs An array with the IDs of the [guilds](https://discord.js.org/#/docs/main/stable/class/Guild) to deploy the commands to.
+   * @returns A promise that resolves once all the commands have been deployed.
+   * @emits `client#commandDeployed`
+   * @emits `client#error`
+   */
   public deployToGuilds(guildIDs: string[]): Promise<void[]> {
     return Promise.all(guildIDs.map((guildID) => this.deployToGuild(guildID)));
   }
 
+  /**
+   * Deploy the commands registered to this deployer's client registry to the testing guild defined
+   * in the client.
+   * @returns A promise that resolves once all the commands have been deployed.
+   * @emits `client#commandDeployed`
+   * @emits `client#error`
+   */
   public deployToTestingGuild(): Promise<void> {
     if (!this.client.testingGuildID) {
       return Promise.reject(new Error('You have not set a testingGuildID for your client. It is recommended to have one set up to automatically deploy slash commands to the testing server.'));
--- src/classes/command/SlashCommandValidator.ts
+++ src/classes/command/SlashCommandValidator.ts
@@ -1,5 +1,9 @@
 import SlashCommand from './SlashCommand';
 
+/**
+ * A validator class to verify if the information of a slash command valid
+ * and can be deployed.
+ */
 class SlashCommandValidator {
   private static readonly NAME_REGEX: RegExp = /^[\P{Lu}\p{N}_-]+$/u;
   private static readonly NAME_MIN: number = 1;
@@ -9,6 +13,15 @@ class SlashCommandValidator {
   private static readonly DESC_MAX: number = 100;
 
 
+  /**
+   * Validate a given slash command.
+   * @param command The slash command to validate.
+   * @throws Throws if command name is too short or too long.
+   * @throws Throws if command name contains invalid characters (i.e. upper-cased characters).
+   * @throws Throws if command description is too short or too long.
+   * @throws Throws if any command alias is too short or too long.
+   * @throws Throws if any command alias contains invalid characters (i.e. upper-cased characters).
+   */
   public static validate(command: SlashCommand): void {
     SlashCommandValidator.validateName(command.name);
     SlashCommandValidator.validateDescription(command.description);
@@ -18,6 +31,12 @@ class SlashCommandValidator {
     }
   }
 
+  /**
+   * Validate the slash command's name.
+   * @param name The name of the slash command.
+   * @throws Throws if command name is too short or too long.
+   * @throws Throws if command name contains invalid characters (i.e. upper-cased characters).
+   */
   private static validateName(name: string): void {
     const { length } = name;
 
@@ -30,6 +49,11 @@ class SlashCommandValidator {
     }
   }
 
+  /**
+   * Validate the slash command's description.
+   * @param description
+   * @throws Throws if command description is too short or too long.
+   */
   private static validateDescription(description: string): void {
     const { length } = description;
 
