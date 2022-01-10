2231117e9cbb11c4aab0e9db3305b60058d561e3
moonstar-x <christian.lopez99@outlook.com>
2022-01-09 22:06:30 -0500

    Created documentation for the new classes and interface.

--- src/classes/locale/GuildLocalizer.ts
+++ src/classes/locale/GuildLocalizer.ts
@@ -1,15 +1,47 @@
-import { Guild } from 'discord.js';
+import Discord from 'discord.js';
 import Localizer from './Localizer';
 
+/**
+ * A class to help with localization of your bot. This handles string translation from the context of the
+ * guild. For more information, check {@link Localizer}.
+ */
 class GuildLocalizer {
+  /**
+   * The main localizer for this guild localizer.
+   * @type {Localizer}
+   * @memberof {GuildLocalizer}
+   */
   public readonly localizer: Localizer;
-  public readonly guild: Guild;
 
+  /**
+   * The guild that corresponds to this guild localizer.
+   * @type {Discord.Guild}
+   * @memberof {GuildLocalizer}
+   */
+  public readonly guild: Discord.Guild;
+
+  /**
+   * The key to be used to save the guild's locale in the client's
+   * data provider.
+   * @type {string}
+   * @default `locale`
+   * @memberof {GuildLocalizer}
+   */
   public readonly dataProviderKey: string;
 
+  /**
+   * The current locale set for the guild. Do not update this value manually, instead use
+   * the `updateLocale()` method.
+   * @type {string}
+   * @memberof {GuildLocalizer}
+   */
   public locale: string;
 
-  constructor(localizer: Localizer, guild: Guild) {
+  /**
+   * @param localizer The main localizer for this guild localizer.
+   * @param guild The guild that corresponds to this guild localizer.
+   */
+  constructor(localizer: Localizer, guild: Discord.Guild) {
     this.localizer = localizer;
     this.guild = guild;
 
@@ -18,6 +50,14 @@ class GuildLocalizer {
     this.locale = localizer.defaultLocale;
   }
 
+  /**
+   * Initializes this guild localizer.
+   * You should call this after initializing the client's data provider since this method
+   * will read the locale set for each guild and will initialize their localizer with that value.
+   * @returns A promise that resolves to the locale set for this guild localizer.
+   * @throws Rejects if a guild localizer was being initialized with an unsupported locale retrieved
+   * from the data provider. This may happen if the locale saved in the data provider was updated manually.
+   */
   public async init(): Promise<string> {
     if (!this.localizer.client.dataProvider) {
       return this.locale;
@@ -35,6 +75,13 @@ class GuildLocalizer {
     return this.locale;
   }
 
+  /**
+   * Updates the locale set to this guild localizer. If the client has a data provider, it will also update
+   * the locale in it for persistence.
+   * @param locale The new locale to be used by this guild localizer.
+   * @returns A promise that resolves once the locale has been updated.
+   * @throws Rejects if the given locale is not supported.
+   */
   public async updateLocale(locale: string): Promise<void> {
     const availableLocales = this.localizer.getAvailableLocales();
     if (!availableLocales.includes(locale)) {
@@ -50,8 +97,18 @@ class GuildLocalizer {
     return this.localizer.client.dataProvider.set(this.guild, this.dataProviderKey, this.locale);
   }
 
-  public t(key: string): string {
-    return this.localizer.translate(key, this.locale);
+  /**
+   * Get the message for the given key translated into the locale of this
+   * guild localizer. You can also supply an object
+   * containing the dynamic values to be used.
+   * @param key The key of the message to translate.
+   * @param values The dynamic values to be replaced in the message.
+   * @returns The translated message.
+   * @throws Throws if the key does not resolve to any message.
+   * @throws Throws if the given locale is not supported.
+   */
+  public t(key: string, values = {}): string {
+    return this.localizer.translate(key, this.locale, values);
   }
 }
 
--- src/classes/locale/Localizer.ts
+++ src/classes/locale/Localizer.ts
@@ -4,13 +4,76 @@ import ExtendedClient from '../ExtendedClient';
 import GuildLocalizer from './GuildLocalizer';
 import LocalizerOptions from '../../interfaces/LocalizerOptions';
 
+/**
+ * A class to help with the localization of your bot. This handles string translation based on the
+ * [intl-messageformat](https://formatjs.io/docs/intl-messageformat/) package. Strings are defined in
+ * [ICU](https://formatjs.io/docs/intl-messageformat/#common-usage-example) format and dynamic values inside.
+ * It is recommended to have a data provider set in the client for the locale settings to be saved
+ * persistently for each guild.
+ */
 class Localizer {
+  /**
+   * The client that this localizer will be used by.
+   * @type {ExtendedClient}
+   * @memberof Localizer
+   */
   public readonly client: ExtendedClient;
+
+  /**
+   * An object that maps the name of the locale to another object that contains
+   * all the available messages mapped by their keys. The messages should follow
+   * a ICU standard, for more information on how to structure these messages, please visit the
+   * [following link](https://formatjs.io/docs/intl-messageformat/#common-usage-example).
+   *
+   * The following is an example of how to structure this object:
+   *
+   * en: {
+   *   'message.test.hello': 'Hello',
+   *   'message.test.bye': 'Bye',
+   *   'message.test.with_value': 'Hello {name}!'
+   * },
+   * es: {
+   *   'message.test.hello': 'Hola',
+   *   'message.test.bye': 'Adios',
+   *   'message.test.with_value': 'Hola {name}!'
+   * },
+   * fr: {
+   *   'message.test.hello': 'Bonjour',
+   *   'message.test.bye': 'Au revoir',
+   *   'message.test.with_value': 'Bonjour {name}!'
+   * }
+   * @type {Record<string, Record<string, string>>}
+   * @memberof {Localizer}
+   */
   public readonly localeStrings: Record<string, Record<string, string>>;
+
+  /**
+   * The default locale to be used in case a guild does not have its locale set in the
+   * client's data provider. You should set a data provider before setting this up so
+   * the guild's locale can be saved persistently.
+   * @type {string}
+   * @memberof {Localizer}
+   */
   public readonly defaultLocale: string;
+
+  /**
+   * The options for this localizer.
+   * @type {LocalizerOptions}
+   * @memberof {Localizer}
+   */
   public readonly options: LocalizerOptions;
+
+  /**
+   * The {@link GuildLocalizer}s for each guild.
+   * @type {Discord.Collection<Discord.Snowflake, GuildLocalizer>}
+   * @memberof {Localizer}
+   */
   public readonly guildLocalizers: Discord.Collection<Discord.Snowflake, GuildLocalizer>;
 
+  /**
+   * @param client The client that this localizer will be used by.
+   * @param options The options for this localizer.
+   */
   constructor(client: ExtendedClient, options: LocalizerOptions) {
     this.client = client;
     this.localeStrings = options.localeStrings;
@@ -19,6 +82,16 @@ class Localizer {
     this.guildLocalizers = new Discord.Collection();
   }
 
+  /**
+   * Initializes all the {@link GuildLocalizer}s for each guild the client is connected to.
+   * You should call this after initializing the client's data provider since this method
+   * will read the locale set for each guild and will initialize their localizer with that value.
+   * In case the client does not have a data provider, you should still call this method, but the
+   * localizers will be initialized with the default locale.
+   * @returns A promise that resolves once all guild localizers are ready.
+   * @throws Rejects if a guild localizer was being initialized with an unsupported locale retrieved
+   * from the data provider. This may happen if the locale saved in the data provider was updated manually.
+   */
   public init(): Promise<string[]> {
     return Promise.all(this.client.guilds.cache.map((guild) => {
       const localizer = new GuildLocalizer(this, guild);
@@ -28,18 +101,45 @@ class Localizer {
     }));
   }
 
+  /**
+   * Get the localizer for the given guild.
+   * @param guild
+   * @returns The guild localizer for the given guild.
+   */
   public getLocalizer(guild: Discord.Guild): GuildLocalizer | undefined {
     return this.guildLocalizers.get(guild.id);
   }
 
+  /**
+   * Get a list of all the available locales retrieved based on the keys of `localeStrings`.
+   * @returns An array containing all the available locales.
+   */
   public getAvailableLocales(): string[] {
     return Object.keys(this.localeStrings);
   }
 
+  /**
+   * Get the message for the given key translated into the given locale. You can also supply an object
+   * containing the dynamic values to be used.
+   * @param key The key of the message to translate.
+   * @param locale The locale to translate the message to.
+   * @param values The dynamic values to be replaced in the message.
+   * @returns The translated message.
+   * @throws Throws if the key does not resolve to any message.
+   * @throws Throws if the given locale is not supported.
+   */
   public translate(key: string, locale?: string | null, values = {}): string {
     return this.getMessage(key, locale || this.defaultLocale).format(values) as string;
   }
 
+  /**
+   * Get the formatter object to format the message of the given key and locale.
+   * @param key The key of the message to translate.
+   * @param locale The locale to translate the message to.
+   * @returns The formatter object for translating the message.
+   * @throws Throws if the key does not resolve to any message.
+   * @throws Throws if the given locale is not supported.
+   */
   private getMessage(key: string, locale: string): IntlMessageFormat {
     const messagesForLocale = this.localeStrings[locale];
 
